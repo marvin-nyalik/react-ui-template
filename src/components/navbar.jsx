6093b2bd1f8da42138b9efedeb7ac6ff39b7dfe4
MARVIN <o.silverygold@gmail.com>
2024-12-15 09:41:50 +0300

    Add holder components

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./styles/Navbar.css";
 
-const navbar = () => {
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>
-      nav
-    </div>
-  )
-}
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          MyLogo
+        </Link>
+        <button className="menu-toggle" onClick={toggleMenu}>
+          {isOpen ? "✖" : "☰"}
+        </button>
+        <ul className={`nav-menu ${isOpen ? "nav-menu-active" : ""}`}>
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Services
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default navbar
+export default Navbar;
